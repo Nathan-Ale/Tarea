a93d696c3b80f6726c6f803a54c728999aa9269d
Jonnathan Alegria <jonathanalegria01@gmail.com>
2025-02-02 20:54:28 -0600

    Nuevo archivo contador añadido

--- Tarea/app/_layout.tsx
+++ Tarea/app/_layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout() {
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
-      router.replace("/form")
+      //router.replace("/form")
     }
   }, [loaded]);
 
